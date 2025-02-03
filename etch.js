589dc08647d66083d39bf248f85ecc6a496194f1
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-02-02 22:27:36 -0500

    Added reset and scaling
    Reset button to prompt user for grid size, clear, reload

--- etch.js
+++ etch.js
@@ -1,14 +1,30 @@
 const containerDiv = document.getElementById("container");
-for (let j =0;j<16;j++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    for (let i =0;i<16;i++){
-        const panel = document.createElement('div');
-        panel.classList.add("panel");
-        panel.addEventListener('mouseover',()=> {
-            panel.style.backgroundColor = 'white';
-        });
-        rowDiv.appendChild(panel);
-    } 
-    containerDiv.appendChild(rowDiv);
+const resetButton = document.getElementById("reset");
+function buildBoard (size) {
+    for (let j = 0;j<size;j++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        for (let i = 0;i<size;i++){
+            const panel = document.createElement('div');
+            panel.classList.add("panel");
+            panel.addEventListener('mouseover',()=> {
+                panel.style.backgroundColor = 'black';
+            });
+            rowDiv.appendChild(panel);
+        } 
+        containerDiv.appendChild(rowDiv);
+    }
 }
+buildBoard (16);
+resetButton.addEventListener('click',()=> {
+    let size = prompt("How large would you like the grid? (10-100)");
+    if (size > 100) alert ("Too big!");
+    else if (size < 10) alert ("Too small!");
+    else if (size <=100 || size >10){
+        while (containerDiv.firstChild){
+            containerDiv.removeChild(containerDiv.firstChild);
+        }
+        buildBoard(size);
+    }
+    else alert("Not a number");
+})
